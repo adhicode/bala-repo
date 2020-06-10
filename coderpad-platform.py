dd1ff689c8eebd3e3c0c69c0b43e8183f468d91f
adhicode <66421143+adhicode@users.noreply.github.com>
2020-06-10 12:08:34 +0530

    coderpad VM platform/arch info

---
+++ coderpad-platform.py
@@ -0,0 +1,14 @@
+#python3
+#platform_os_info.py
+
+import platform
+
+print('uname:', platform.uname())
+
+print()
+print('system   :', platform.system())
+print('node     :', platform.node())
+print('release  :', platform.release())
+print('version  :', platform.version())
+print('machine  :', platform.machine())
+print('processor:', platform.processor())
