163e3c16fe6efb0fd851112f0b69b11a1c8f5372
adhicode <66421143+adhicode@users.noreply.github.com>
2020-06-22 23:33:47 +0530

    Update sll-reverse.c

--- sll-reverse.c
+++ sll-reverse.c
@@ -13,14 +13,14 @@ void print_list(Node *root){
 }
 
 Node* reverse(Node *current){
-  Node *last = NULL;
+  Node *prev = NULL;
   Node *next = NULL;
   Node *ret;
   
   while(current){
     next = current->next; // next points to 2
-    current->next = last; // 1 points to null
-    last = current; // last points to 1
+    current->next = prev; // 1 points to null
+    prev = current; // prev points to 1
     ret = current;
     current = next; // current points to 2
   }
@@ -33,9 +33,16 @@ int main(){
  Node n1 = {0, 1};
  Node n2 = {&n1, 2};
  Node n3 = {&n2, 3};
+ Node root[3] = {
+                  {&root[1],1}, 
+                  {&root[2],2}, 
+                  {NULL,3}
+                  };
  
- Node *root = &n3;
- //print_list(root);
- Node *result = reverse(&n3);
+ //Node *root = &n3;
+ print_list(root);
+  printf("root& %x, root[0]& %x, root[1]& %x, root[2]& %x \n", root, &root[0], &root[1], &root[2]);
+ //Node *result = reverse(&n3);
+ Node *result = reverse(&root[0]);
  print_list(result);
 }
